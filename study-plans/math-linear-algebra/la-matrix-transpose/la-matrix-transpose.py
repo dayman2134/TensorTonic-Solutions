0e653f236d38c36cd1341fb25de7652583f9ed08
dayman2134 <troypinkney02@gmail.com>
2026-05-20 12:03:05 -0400

    Update solution for la-matrix-transpose

--- study-plans/math-linear-algebra/la-matrix-transpose/la-matrix-transpose.py
+++ study-plans/math-linear-algebra/la-matrix-transpose/la-matrix-transpose.py
@@ -4,4 +4,14 @@ def matrix_transpose(A):
     """
     Returns: ndarray, the transpose of A.
     """
-    return np.array(A).T
+    A = np.array(A)
+    rows = A.shape[0]
+    cols = A.shape[1]
+
+    T = np.zeros((cols, rows))
+
+    for i in range(rows):
+        for j in range(cols):
+            T[j][i] = A[i][j]
+
+    return T
